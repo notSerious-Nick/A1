4c6c183c435a8a05985da93f48fa91dfd7d16cd5
nick <nick96069@gmail.com>
2026-03-26 22:35:56 -0600

    A7 1

--- A7/startup/static/restaurant/dashboard.js
+++ A7/startup/static/restaurant/dashboard.js
@@ -75,6 +75,8 @@ async function loadOrders() {
     });
     filterCards();
   } catch (error) {
+    let fetchedOrders = document.querySelector("#fetched-orders");
+    let fetchError = document.querySelector("#fetch-error");
     fetchedOrders.textContent = "";
     fetchError.textContent = "Cannot load orders";
   }
